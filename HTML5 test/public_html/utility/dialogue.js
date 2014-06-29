681bf1ac24fcd5a504f6218dc25a010d0ef2abc7
rasmusoh <rasmusoh@kth.se>
2014-06-29 16:33:24 +0200

    dialogue class fix

--- HTML5 test/public_html/utility/dialogue.js
+++ HTML5 test/public_html/utility/dialogue.js
@@ -1,36 +1,40 @@
 
-function Dialogue(x,y){
-    this.initialize(x,y)
-
+function Dialogue(x,y, font)
+{
+    this.initialize(x,y, font)
 }
 
 
-Dialogue.prototype.initialize = function(x,y)
+Dialogue.prototype.initialize = function(x,y, font)
 {
     this.container = new createjs.Container();
     this.previousY = 0;
     this.container.x = x;
     this.container.y = y;
+    this.font = font;
 }
 
 Dialogue.prototype.addOption = function(name, ifClicked)
 {
     start = new createjs.Text();
     start.text = name;
-    start.font = "96px Oswald";
+    start.font = this.font;
     start.color = "#FF7700";
     start.x = 0;
-    start.y = this.previousY+start.getMeasuredHeight()
-//    rekt = start.getBounds();
-//    var shape = new createjs.Shape();
-//    shape.graphics.beginFill("#ff0000").drawRect(rekt);
-//    shape.aplha = 0.5;
-//    this.container.addChild(rekt,start);
-    //shape.addEventListener("click",ifClicked);
+    start.y = this.previousY;
+    rekt = start.getBounds();
+    var shape = new createjs.Shape();
+    shape.graphics.beginFill("#ff0000").drawRect(rekt.x,rekt.y+this.previousY+
+            rekt.height/3,rekt.width,rekt.height);
+    shape.alpha = 0.01;
+    this.container.addChild(shape,start);
+    shape.addEventListener("click",ifClicked);
+    this.previousY += rekt.height*1.5;
 }
 
 Dialogue.prototype.getDialogue = function()
 {
+    console.log('getDialogue');
     return this.container;
 }
 
