113dc86463cd54a343004774af3638e8f9bdef24
a-morn <albin.morner@gmail.com>
2014-07-25 22:59:42 +0200

    useorret .js

--- HTML5 test/public_html/Application Control.js
+++ HTML5 test/public_html/Application Control.js
@@ -1,14 +1,10 @@
 function gogogo ()
  {        
-     //Menu.init();
-   //Interior.init("first",{"fader":fadeToFromBlack});
    
-   topplefight.init();
-    //spotfight.init();
-//    view = new View();   
-//    ctrls = new Controls();
-//    model = new Model();
-//    presenter = new Presenter();
-//    presenter.Init(view, ctrls, model);
+    view = new View();   
+    ctrls = new Controls();
+    model = new Model();
+    presenter = new Presenter();
+    presenter.Init(view, ctrls, model);
  }
  
